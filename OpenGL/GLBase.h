1060ec74d4df2db7c873c47bedd71c283fcaa016
ssss3301 <wangm_2009@163.com>
2019-06-14 14:50:06 +0800

    Add project files.

---
+++ OpenGL/GLBase.h
@@ -0,0 +1,26 @@
+#ifndef __GL_BASE_H__
+#define __GL_BASE_H__
+
+class GLBase {
+public:
+	GLBase() {
+
+	}
+
+	virtual ~GLBase() {
+
+	}
+
+	virtual void init_gl_environment() {
+
+	}
+
+	virtual void render() {
+
+	}
+
+private:
+
+};
+
+#endif //__GL_BASE_H__
